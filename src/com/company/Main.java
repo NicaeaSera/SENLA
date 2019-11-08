eb9592e8110958e6c0858ad5d52794506a3eb7ab
Liza G <Lizagod2000@mail.ru>
2019-11-09 00:44:46 +0300

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,7 +33,7 @@ public class Main {
             segm.solve();
         }
 
-        if(task_number == 4)
+        if(task_number == 36)
         {
             System.out.print("Enter A and B to display GCD and ICM" + '\n');
             int A = sc.nextInt();
